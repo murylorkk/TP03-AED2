3f784be750fde478e38409d63b181cf89b461cd1
vitoriafarias19 <vitoria.farias@icomp.ufam.edu.br>
2026-06-14 12:03:39 -0400

    Update TAD_arquivo.c

--- src/main/TAD_arquivo.c
+++ src/main/TAD_arquivo.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "TAD_arquivo.h"
 
+double tempo_agora(void) {
+    return (double) clock() / CLOCKS_PER_SEC;
+}
+
 int ler_registro_disco(FILE *arq, long num_reg, Registro *reg) {
-    if (fseek(arq, num_reg * (long)sizeof(Registro), SEEK_SET) != 0){ 
+    if (fseek(arq, num_reg * (long)sizeof(Registro), SEEK_SET) != 0) { 
         return 0;
     }
+
     return fread(reg, sizeof(Registro), 1, arq) == 1;
 }
 
@@ -17,49 +23,81 @@ long total_registros(FILE *arq) {
 
 void gerar_arquivo(int n) {
     int *mats = malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++){
+
+    if (mats == NULL) {
+        fprintf(stderr, "Erro de memoria.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < n; i++) {
         mats[i] = 10000 + i;
     }
 
-    // embaralha as matrículas para não ficarem em ordem sequencial no disco
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        int tmp = mats[i]; mats[i] = mats[j]; mats[j] = tmp;
+
+        int tmp = mats[i];
+        mats[i] = mats[j];
+        mats[j] = tmp;
     }
 
     const char *cursos[] = {"CC", "EC", "SI", "ES", "MAT"};
+
     FILE *arq = fopen(ARQUIVO, "wb");
     
-    if (arq == NULL){
-        fprintf(stderr, "Erro ao criar arquivo.\n"); exit(1);
-     }
+    if (arq == NULL) {
+        fprintf(stderr, "Erro ao criar arquivo.\n");
+        free(mats);
+        exit(1);
+    }
 
     for (int i = 0; i < n; i++) {
         Registro r;
+
         memset(&r, 0, sizeof(r));
-        r.matricula    = mats[i];
-        r.cr           = 2.0 + ((double)rand() / RAND_MAX) * 8.0;
+
+        r.matricula = mats[i];
+        r.cr = 2.0 + ((double) rand() / RAND_MAX) * 8.0;
         r.ano_ingresso = 1985 + rand() % 40;
+
+        snprintf(r.nome, TAM_NOME, "Aluno_%05d", r.matricula);
         snprintf(r.curso, TAM_CURSO, "%s", cursos[rand() % 5]);
+
         fwrite(&r, sizeof(r), 1, arq);
     }
 
     fclose(arq);
     free(mats);
+
     printf("Arquivo gerado: %d registros, %zu bytes cada.\n", n, sizeof(Registro));
 }
 
 void sortear_chaves(FILE *arq, int *chaves, int qtd, long total) {
     long *idx = malloc(total * sizeof(long));
-    for (long i = 0; i < total; i++) idx[i] = i;
+
+    if (idx == NULL) {
+        fprintf(stderr, "Erro de memoria.\n");
+        exit(1);
+    }
+
+    for (long i = 0; i < total; i++) {
+        idx[i] = i;
+    }
+
     for (int i = 0; i < qtd; i++) {
         long j = i + rand() % (total - i);
-        long tmp = idx[i]; idx[i] = idx[j]; idx[j] = tmp;
+
+        long tmp = idx[i];
+        idx[i] = idx[j];
+        idx[j] = tmp;
     }
+
     Registro r;
+
     for (int i = 0; i < qtd; i++) {
         ler_registro_disco(arq, idx[i], &r);
         chaves[i] = r.matricula;
     }
+
     free(idx);
 }
