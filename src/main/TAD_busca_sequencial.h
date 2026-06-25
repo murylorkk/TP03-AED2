1d64c015d3827abbde0fb168a787c29d4b114f5a
Murilo Duarte Bittencourt <murilo.bittencourt@icomp.ufam.edu.br>
2026-06-25 07:15:21 -0400

    deletando src/main/TAD_busca_sequencial.h

--- src/main/TAD_busca_sequencial.h
+++
@@ -1,9 +0,0 @@
-#ifndef TAD_BUSCA_SEQUENCIAL_H
-#define TAD_BUSCA_SEQUENCIAL_H
-
-#include <stdio.h>
-#include "TAD_arquivo.h"
-
-long busca_sequencial_chave(FILE *arq, int chave_buscada, Registro *reg_encontrado);
-
-#endif
